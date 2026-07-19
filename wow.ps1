e179f2803f036d17acd4e4d8bb74ad199ef2ca00
LovecheatsLOL <Redrobotdhli@gmail.com>
2026-07-19 21:24:51 +0300

    Create wow.ps1

---
+++ wow.ps1
@@ -0,0 +1,8 @@
+Add-Type -AssemblyName PresentationFramework
+
+[System.Windows.MessageBox]::Show(
+    "Hussain, you got hecked plis pay Platoon 100 dolla dolaa 😂",
+    "Totally Real Hacker",
+    "OK",
+    "Warning"
+)
